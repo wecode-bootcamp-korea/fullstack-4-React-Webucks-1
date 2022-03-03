5b7eede5c6cfa0cdd121443c61e1f5dbff7175b5
shon97 <wjdgns200297@gmail.com>
2022-03-03 11:38:25 +0900

    Add : Mission Completed

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import React from "react";
-import Loginyoungseo from "./pages/kimyoungseo/pages/Login/Login";
-import Listyoungseo from "./pages/kimyoungseo/pages/List/List";
-import Detailyoungseo from "./pages/kimyoungseo/pages/Detail/Detail";
 import Main from "./pages/Main";
+import LoginSeongho from "../src/pages/seonghoson/pages/login/Login";
+import ListSeongho from "../src/pages/seonghoson/pages/list/List";
+import DetailSeongho from "../src/pages/seonghoson/pages/detail/Detail";
+
 import "./styles/reset.scss";
+import "./styles/common.scss";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Main />} />
-        <Route path="/login-youngseo" element={<Loginyoungseo />} />
-        <Route path="/list-youngseo" element={<Listyoungseo />} />
-        <Route path="/detail-youngseo" element={<Detailyoungseo />} />
+        <Route path="login-seonghoson" element={<LoginSeongho />} />
+        <Route path="/list-seonghoson" element={<ListSeongho />} />
+        <Route path="/detail-seonghoson/:id" element={<DetailSeongho />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/seonghoson/components/coffee/Coffee.js
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { BsHeart, BsHeartFill } from "react-icons/bs";
+
+import "./Coffee.scss";
+
+let likeData = JSON.parse(localStorage.getItem("likes"));
+
+function Coffee(props) {
+  const { item } = props;
+  const navigate = useNavigate();
+
+  const [isLike, setIsLike] = useState(likeData.includes(item.id));
+
+  function heartBtnClick(bool) {
+    let likeData = JSON.parse(localStorage.getItem("likes"));
+    if (likeData.includes(item.id)) {
+      likeData = likeData.filter((id) => {
+        if (id !== item.id) return id;
+        else return null;
+      });
+    } else {
+      likeData.push(item.id);
+    }
+    localStorage.setItem("likes", JSON.stringify(likeData));
+    setIsLike(bool);
+  }
+
+  return (
+    <div className="coffee_main">
+      {isLike ? (
+        <BsHeartFill
+          onClick={() => heartBtnClick(false)}
+          className="icon icon_heart"
+          color="red"
+        />
+      ) : (
+        <BsHeart
+          onClick={() => heartBtnClick(true)}
+          className="icon icon_heart"
+          color="white"
+        />
+      )}
+      <picture
+        className="image_wrapper"
+        onClick={() => {
+          navigate(`/detail-seonghoson/${item.id}`, { state: { item } });
+        }}
+      >
+        <img alt={item.name} src={item.image} />
+      </picture>
+      <span className="coffee_name">{item.name}</span>
+    </div>
+  );
+}
+
+export default Coffee;
