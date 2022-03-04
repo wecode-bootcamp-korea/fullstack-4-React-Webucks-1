15e8e406b44fb4e1e971b4ee32ee66fc6af1c8e0
shw779 <shw779@naver.com>
2022-03-04 09:59:47 +0900

    [refactoring]id, pwd 정규식 검사로 변경

--- src/pages/seulhaewon/Login/Login.js
+++ src/pages/seulhaewon/Login/Login.js
@@ -1,4 +1,3 @@
-
 import './Login.scss';
 import "../../../styles/reset.scss";
 import { useNavigate, Link } from "react-router-dom";
@@ -8,7 +7,8 @@ import { faEyeSlash, faEye} from '@fortawesome/free-solid-svg-icons';
 
 function Login() {
   const navigate = useNavigate();
-  
+  const idReg = /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/;
+  const pwdReg = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/;
   const [inputid, setId] = useState("");
   const [inputPwd, setPwd] = useState("");
   const [pwdStatus, setPwdStatus] = useState({
@@ -18,13 +18,20 @@ function Login() {
 
   const HandleIdInput = event =>{
     setId(event.target.value);
-    
   }
   const HandlePwdInput = event =>{
     setPwd(event.target.value);
   }
   const ButtonChange = (trueValue, falseValue) =>{
-    return (inputid.indexOf('@')!==-1 && inputPwd.length>4) ? trueValue:falseValue;
+    return (inputid.match(idReg) && inputPwd.match(pwdReg)) ? trueValue:falseValue;
+  }
+  const BorderChange = (flag, trueValue, falseValue) =>{
+    if(flag === "id")
+      return (inputid.match(idReg)) ? trueValue : falseValue;
+    else if(flag === "pwd")
+      return (inputPwd.match(pwdReg)) ? trueValue : falseValue;
+    else
+      return false;
   }
   const goToMain = () => {
     navigate("/list-haewon");
@@ -40,8 +47,9 @@ function Login() {
         <section className="container">
             <div id="login_box">
                 <div className="webucks">webucks</div>
-                <input id="id" type="text" placeholder="전화번호,사용자 이름 또는 이메일" onChange={HandleIdInput}/>
-                <div className="pwdBox"><input id="pwd" type={pwdStatus.type} placeholder="비밀번호" onChange={HandlePwdInput}/>
+                <input id="id" className={BorderChange("id","idBorderGreen","idBorderDefault")} type="text" placeholder="전화번호,사용자 이름 또는 이메일" onChange={HandleIdInput}/>
+                <div className="pwdBox">
+                    <input id="pwd" className={BorderChange("pwd","pwdBorderGreen","pwdBorderDefault")} type={pwdStatus.type} placeholder="비밀번호" onChange={HandlePwdInput}/>
                     <FontAwesomeIcon icon={pwdStatus.icon} className="fa-eye-slash" onClick={changeEye}/>
                 </div>
                 <button className={ButtonChange("activeBtn","inactiveBtn")} 
