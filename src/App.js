bc1935f12f6e664bf82a404429e34071e955b513
eui <wlsun0324@gmail.com>
2022-03-07 12:26:39 +0900

    Add : login,list,detail 페이지 및 기능 구현

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import React from "react";
 import Loginyoungseo from "./pages/kimyoungseo/pages/Login/Login";
 import Listyoungseo from "./pages/kimyoungseo/pages/List/List";
 import Detailyoungseo from "./pages/kimyoungseo/pages/Detail/Detail";
+import Logineuitaek from "./pages/leeeuitaek/Login/Login";
+import Listeuitaek from "./pages/leeeuitaek/List/List";
+import Detaileuitaek from "./pages/leeeuitaek/Detail/Detail";
 import Main from "./pages/Main";
 import "./styles/reset.scss";
 
@@ -14,6 +17,9 @@ function App() {
         <Route path="/login-youngseo" element={<Loginyoungseo />} />
         <Route path="/list-youngseo" element={<Listyoungseo />} />
         <Route path="/detail-youngseo" element={<Detailyoungseo />} />
+        <Route path="/login-euitaek" element={<Logineuitaek />} />
+        <Route path="/list-euitaek" element={<Listeuitaek />} />
+        <Route path="/detail-euitaek" element={<Detaileuitaek />} />
       </Routes>
     </BrowserRouter>
   );
